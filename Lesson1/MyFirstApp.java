35e7993ba8f6431f09204d2e32563e9e24d2ccf5
Sergey Savintsev <ssspiter@gmail.com>
2020-05-17 18:56:32 +0300

    первая версия

---
+++ Lesson1/MyFirstApp.java
@@ -0,0 +1,9 @@
+public class MyFirstApp {
+    
+    public static void main (String[] args) {
+
+    System.out.println("Hello, world");
+
+    }
+
+}
---
+++ Lesson1/Variable.java
@@ -0,0 +1,23 @@
+public class Variable 
+{
+	public static void main (String[] args) 
+	{
+		byte numberOfCores = 2;
+		short processorSpeed = 2000;
+		int cashL2PerCore = 256;
+		long cashL3 = 4;
+		float display = 13.3f;
+		double storage = 250.69;
+		char serialNumberFirstLetter = 'C';
+		boolean myNotebook = true;
+
+		System.out.println ("Количество ядер процессора = " + numberOfCores);
+		System.out.println ("Частота процессора = " + processorSpeed);
+		System.out.println ("Размер кэша L2 = " + cashL2PerCore);
+		System.out.println ("Размер кэша L3 = " + cashL3);
+		System.out.println ("Диагональ экрана = " + display);
+		System.out.println ("Объем HDD = " + storage);
+		System.out.println ("Серийный номер начинается с '" + serialNumberFirstLetter +"'");
+		System.out.println ("Этот ноутбук Сергея = " + myNotebook);
+	}
+}
