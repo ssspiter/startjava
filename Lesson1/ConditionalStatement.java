cedfeb934d85e04ba805076423240732a78b9905
Sergey Savintsev <ssspiter@gmail.com>
2020-05-17 23:50:56 +0300

    задание №1 - if

---
+++ Lesson1/ConditionalStatement.java
@@ -0,0 +1,40 @@
+public class ConditionalStatement {
+
+	public static void main(String[] args) {
+	
+	int age = 44;
+	String gender = "man";
+	float height = 1.72f;
+	char firstNameChar = 'S';
+
+	if (age > 20){
+		System.out.println ("Ура, ваш возраст больше 20");
+	}
+
+	if (gender == "man"){
+		System.out.println ("Вы мужчина, и это прекрасно");
+	}
+
+	if (gender != "man") {
+		System.out.println ("Вы женщина, и это великолепно");
+	}
+
+	if (height < 1.8) {
+		System.out.println ("Вы не высокого роста, это ваше преимущество");
+	}
+	else {
+		System.out.println ("Вы высокого роста, это ваше преимущество");
+	}
+
+	if (firstNameChar == 'M') {
+		System.out.println ("Первая буква вашего имени - M");
+	}
+	else if (firstNameChar == 'I') {
+		System.out.println ("Первая буква вашего имени - I");
+	}
+	else {
+		System.out.println ("Ваше имя не начинается ни с M, ни с I");
+	}
+
+	}
+}
