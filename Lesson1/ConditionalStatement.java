e430b0350b89aa3832b68d282befd999f66111ec
Sergey Savintsev <ssspiter@gmail.com>
2020-05-18 20:44:11 +0300

    исправлены ошибки

--- Lesson1/ConditionalStatement.java
+++ Lesson1/ConditionalStatement.java
@@ -1,39 +1,36 @@
 public class ConditionalStatement {
 
 	public static void main(String[] args) {
-	int age = 44;
-	String gender = "man";
-	float height = 1.72f;
-	char firstNameChar = 'S';
+		
+		int age = 44;
+		if (age > 20) {
+			System.out.println ("Ура, ваш возраст больше 20");
+		}
 
-	if (age > 20){
-		System.out.println ("Ура, ваш возраст больше 20");
-	}
+		String gender = "man";
+		if (gender == "man") {
+			System.out.println ("Вы мужчина, и это прекрасно");
+		}
+	
+		if (gender != "man") {
+			System.out.println ("Вы женщина, и это великолепно");
+		}
 
-	if (gender == "man"){
-		System.out.println ("Вы мужчина, и это прекрасно");
-	}
-	
-	if (gender != "man") {
-		System.out.println ("Вы женщина, и это великолепно");
-	}
-	
-	if (height < 1.8) {
-		System.out.println ("Вы не высокого роста, это ваше преимущество");
-	}
-	else {
-		System.out.println ("Вы высокого роста, это ваше преимущество");
-	}
-	
-	if (firstNameChar == 'M') {
-		System.out.println ("Первая буква вашего имени - M");
-	}
-	else if (firstNameChar == 'I') {
-		System.out.println ("Первая буква вашего имени - I");
-	}
-	else {
-		System.out.println ("Ваше имя не начинается ни с M, ни с I");
-	}
+		float height = 1.72f;
+		if (height < 1.8) {
+			System.out.println ("Вы не высокого роста, это ваше преимущество");
+		} else {
+			System.out.println ("Вы высокого роста, это ваше преимущество");
+		}
 
+		char firstNameChar = 'S';
+		if (firstNameChar == 'M') {
+			System.out.println ("Первая буква вашего имени - M");
+		}
+		else if (firstNameChar == 'I') {
+			System.out.println ("Первая буква вашего имени - I");
+		} else {
+			System.out.println ("Ваше имя не начинается ни с M, ни с I");
+		}
 	}
 }
--- Lesson1/Cycle.java
+++ Lesson1/Cycle.java
@@ -1,34 +1,31 @@
 public class Cycle {
 
 	public static void main(String[] args) {
+	
 		int i;
-		int m = 6;
-		int n = 10;
-		int l = 0;
-
 		for (i = 0; i <= 20; i++) {
-			System.out.print (i + " ");
+			System.out.print(i + " ");
 		}
-		System.out.println ();
-
+		System.out.println();
+		
+		int m = 6;
 		while (m >= -6) {
-			System.out.print (m + " ");
+			System.out.print(m + " ");
 			m -= 2;
 		}
-		System.out.println ();
-
+		System.out.println();
+		
+		int n = 10;
+		int l = 0;
 		do {
-            if (n%2 == 0) {
+			if (n % 2 == 0) {
                 n++;
-            }
-            else {
+            } else {
                 l += n;
                 n++;
             }
         }
         while (n <= 20);
         System.out.println(l);
-		
 	}
-
 }
